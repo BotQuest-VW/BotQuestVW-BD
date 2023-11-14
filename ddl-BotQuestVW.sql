61431d94dccc604f9281c99b356ef4abb2b03281
juliaathar <juathar@gmail.com>
2023-11-14 20:18:04 -0300

    :tada: init

---
+++ ddl-BotQuestVW.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE bd_bqvw;
+
+USE bd_bqvw;
+
+CREATE TABLE tb_setor(
+    id BINARY(16) NOT NULL,
+    cod_setor TINYINT NOT NULL,
+   nome VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tb_usuario(
+	id BINARY(16) NOT NULL,
+    id_setor BINARY(16) NOT NULL,
+    tipo_usuario TINYINT NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    vwId INT NOT NULL UNIQUE,
+    dataNascimento DATETIME NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY ( id_setor) REFERENCES tb_setor(id)
+);
+
+CREATE TABLE tb_pergunta(
+	id BINARY(16) NOT NULL,
+     id_setor BINARY(16) NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY ( id_setor) REFERENCES tb_setor(id)
+);
+
+CREATE TABLE tb_chamado(
+	id BINARY(16) NOT NULL,
+    id_usuario BINARY(16) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    situacao TINYINT NOT NULL,
+    data_chamado DATETIME NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_usuario) REFERENCES tb_usuario(id)
+);
